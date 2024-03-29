f7483758a2e604b737b730b46163c74cba358d26
ChuaTeSimp <nhatdi2710@gmail.com>
2024-03-30 01:28:01 +0700

    hoan thanh delete.php

--- public/pages/admin/delete.php
+++ public/pages/admin/delete.php
@@ -1,12 +1,25 @@
 <?php
 // Đưa vào tệp kết nối cơ sở dữ liệu PDO
 require __DIR__ . '/../../../src/db/db_connect.php';
-
+$thongbao = '';
 // Kiểm tra xem ID sách đã được cung cấp qua URL hay chưa
 if (isset($_GET['id']) && !empty($_GET['id'])) {
     $id = $_GET['id'];
-
+    $tenSach = '';
     try {
+        // lấy tên sách trước khi xóa.
+        $stmt = $pdo->prepare("SELECT TEN_SACH FROM SACH WHERE MA_SACH = :id");
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            $tenSach = $stmt->fetch(PDO::FETCH_ASSOC)['TEN_SACH'];
+        } else {
+            $thongbao = "<p>Không tìm thấy sách với ID $id.</p>";
+            echo $thongbao;
+            exit;
+        }
+
         // Chuẩn bị truy vấn SQL để xóa sách
         $query = "DELETE FROM SACH WHERE MA_SACH = :id";
         $stmt = $pdo->prepare($query);
@@ -19,16 +32,20 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
 
         // Kiểm tra xem có sách nào được xóa không
         if ($stmt->rowCount() > 0) {
-            echo "<p>Sách với ID $id đã được xóa thành công.</p>";
+            $thongbao = "<p class='success-message'>Sách: $tenSach đã được xóa thành công.</p>";
         } else {
-            echo "<p>Không tìm thấy sách với ID $id để xóa.</p>";
+            $thongbao =  "<p>Không tìm thấy sách với ID $id để xóa.</p>";
+            echo $thongbao;
             exit;
         }
     } catch (PDOException $e) {
-        die("Có lỗi xảy ra khi xóa sách: " . $e->getMessage());
+
+        $thongbao = "Có lỗi xảy ra khi xóa sách: " . $e->getMessage();
+        die($thongbao);
     }
 } else {
-    echo "<p>ID sách không được cung cấp hoặc không hợp lệ.</p>";
+    $thongbao = "<p>ID sách không được cung cấp hoặc không hợp lệ.</p>";
+    echo $thongbao;
     exit;
 }
 
@@ -36,4 +53,21 @@ if (isset($_GET['id']) && !empty($_GET['id'])) {
 $pdo = null;
 
 // Cung cấp một liên kết để quay lại trang quản lý sách
-echo '<a class="nav-link" href="/admin/trangchu">Quay lại danh sách sách</a>';
+//echo '<a class="nav-link" href="/admin/quanly">Quay lại danh sách sách</a>';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<!-- Header -->
+<?php require __DIR__ . '/../../../src/partials/header.php' ?>
+
+<body>
+    <!-- Sub-Header -->
+    <?php require __DIR__ . '/../../../src/partials/sub-header--admin.php' ?>
+
+    <main id="delete-page" class="container text-center mt-4">
+        <h3 style="color: #86A789;"><?= $thongbao; ?></h3>
+        <a class="nav-link" href="/admin/quanly">Quay lại danh sách sách</a>
+    </main>
+</body>
+
+</html>
